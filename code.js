356ccc40d03f725a7f30c9c24c7737087fcffcf0
Roberto Nascimento Lucena <79461192+RobertoLucena@users.noreply.github.com>
2021-02-27 13:53:26 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,49 @@
+i = 0;
+
+/*function dimc() {
+    var clickbutton1 = document.getElementById("entrevistas")
+    clickbutton1[0].innerHTML = i - 1
+    i--
+}*/
+
+function dimc() {
+    i--
+    document.getElementById("b1").innerHTML = i
+}
+
+
+/*function aumc() {
+    var clickbutton1 = document.getElementById("entrevistas")
+    clickbutton1[0].innerHTML = i
+    i++
+}*/
+
+function aumc() {
+    i++
+    document.getElementById("b1").innerHTML = i
+}
+
+x = 0;
+
+/*function dime() {
+    var clickbutton2 = document.getElementById("curriculos")
+    clickbutton2[0].innerHTML = x - 1
+    x--
+}*/
+
+function dime() {
+    x--
+    document.getElementById("b2").innerHTML = x
+}
+
+
+/*function aume() {
+    var clickbutton2 = document.getElementById("curriculos")
+    clickbutton2[0].innerHTML = x
+    x++
+}*/
+
+function aume() {
+    x++
+    document.getElementById("b2").innerHTML = x
+}
